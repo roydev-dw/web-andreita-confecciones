3247dac060a86f2725e7b8b3d4aea306131c272c
roydev-dw <ado.pezzini@gmail.com>
2024-09-21 20:41:38 -0300

    html y css ok

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+document.getElementById('agregar-carrito').addEventListener('click', function() {
+  window.location.href = '../pages/servicios.html';
+});
 
-function Producto (id, nombre,descripcion, precio, talla, stock){
+
+
+
+/* function Producto (id, nombre,descripcion, precio, talla, stock){
   this.id = id,
   this.nombre = nombre,
   this.descripcion = descripcion,
@@ -11,4 +17,4 @@ function Producto (id, nombre,descripcion, precio, talla, stock){
 let producto1 = new Producto(1,'delantal', 'Esto es lo ultimo en moda', 15000, 'XL', 20)
 let producto2 = new Producto(2,'scrunchies', 'Los colets mas hermosos que podras encontrar', 2500, 'estandar', 20)
 
-console.log(Producto);
+console.log(Producto); */
